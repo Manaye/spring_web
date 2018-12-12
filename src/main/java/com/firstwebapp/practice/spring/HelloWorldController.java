b9bde804289b46da55cba883aa2498d573c4d55d
Manaye <hiwotmanaye2@gmail.com>
2018-12-11 21:49:31 -0800

    capitalize

--- src/main/java/com/firstwebapp/practice/spring/HelloWorldController.java
+++ src/main/java/com/firstwebapp/practice/spring/HelloWorldController.java
@@ -1,22 +1,23 @@
 package com.firstwebapp.practice.spring;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloWorldController {
-    @ResponseBody
-    @RequestMapping("/")
-    public String helloWorld(){
+   @ResponseBody
+    @RequestMapping(value = "/hello" ,method = RequestMethod.GET)
+    public String helloWorld(Model m){
         return "Hello world!";
     }
-
-    @RequestMapping("/capitalize/:word")
-    public void  capitalize(RequestParam r){
-        System.out.println(r.value().toUpperCase());
+    @ResponseBody
+    @RequestMapping(value = "/capitalize/{string}",method = RequestMethod.GET)
+    public String capitalizeString(@PathVariable() String string, Model m) {
+        return capitalizeString(string);
+    }
+    public static String capitalizeString(String string){
+        return string.toUpperCase();
     }
 
 }
--- src/main/java/com/firstwebapp/practice/spring/PracticeSpringApplication.java
+++ src/main/java/com/firstwebapp/practice/spring/PracticeSpringApplication.java
@@ -3,6 +3,12 @@ package com.firstwebapp.practice.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+
 @SpringBootApplication
 public class PracticeSpringApplication {
 
